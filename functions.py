11028d860d4114e9bcc4fc67053640829a33ae70
Anderson <andersonacostam@gmail.com>
2021-05-11 17:06:52 -0300

    Tests prime_num, convert - OK

--- functions.py
+++ functions.py
@@ -1,7 +1,66 @@
-def ssum(number):
+def ssum(number) -> int:
     if isinstance(number, int or float):
         return number + 4
     else:
         if number.isdigit():
             return int(number) + 4
     return 0
+
+
+def prime_numbers(end: int) -> []:
+    numbers = []
+    for num in range(1, end + 1):
+        if num != 1 and not any([num % div == 0 for div in range(2, num)]):
+            numbers.append(num)
+    return numbers
+
+
+def convert_binary_to_decimal(binnary) -> int:
+    decimal = 0
+
+    if isinstance(binnary, int):
+        binnary = str(binnary)
+
+    if isinstance(binnary, str):
+        if binnary.isnumeric():
+            if all(elem in ["0", "1"] for elem in binnary):
+                binnary = binnary[::-1]
+                for exp in range(0, len(binnary)):
+                    if binnary[exp] == "1":
+                        decimal += pow(2, exp)
+
+    return decimal
+
+
+def convert_decimal_to_binary(decimal, complement: bool = False) -> str:
+    binnary = ""
+    qnt_bits = 8
+
+    if isinstance(decimal, str):
+        if decimal.isnumeric():
+            decimal = int(decimal)
+        else:
+            return "0"
+
+    if isinstance(decimal, int):
+        if decimal > 255:
+            qnt_bits = 16
+
+        if decimal <= 65535:
+            while decimal > 0:
+                rest = decimal % 2
+                decimal = decimal // 2
+
+                if rest:
+                    binnary += "1"
+                else:
+                    binnary += "0"
+
+            binnary = binnary[::-1]
+        else:
+            return "0"
+
+    if complement:
+        binnary = binnary.rjust(qnt_bits, "0")
+
+    return binnary
--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,10 @@
 import pytest
-from functions import ssum
+from functions import (
+    ssum,
+    prime_numbers,
+    convert_binary_to_decimal,
+    convert_decimal_to_binary,
+)
 
 
 @pytest.fixture
@@ -16,7 +21,28 @@ def test_convert_to_int(word):
         int(word) == 0
 
 
-def test_functions():
+def test_function_ssum():
     assert ssum(4) == 8
     assert ssum("33") == 37
     assert ssum("a3i") == 0
+
+
+def test_function_prime_numbers():
+    assert 97 in prime_numbers(100)
+
+
+def test_convert_binary_to_decimal():
+    assert convert_binary_to_decimal("10001") == 17
+    assert convert_binary_to_decimal("i0001") == 0
+    assert convert_binary_to_decimal(101) == 5
+    assert convert_binary_to_decimal(102) == 0
+    assert convert_binary_to_decimal("20001") == 0
+
+
+def test_function_convert_decimal_to_binary():
+    assert convert_decimal_to_binary(210) == "11010010"
+    assert convert_decimal_to_binary("310") == "100110110"
+    assert convert_decimal_to_binary(-47, True) == "00000000"
+    assert convert_decimal_to_binary("-98i") == "0"
+    assert convert_decimal_to_binary("31094") == "111100101110110"
+    assert convert_decimal_to_binary("65536") == "0"
